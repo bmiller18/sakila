72097d21fe669cf305d90916d5d1e75ed9f7ddd5
Brittany Miller <bmiller012712@gmail.com>
2020-08-05 15:45:59 -0400

    First Version

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using sakila.Models;
 
 namespace sakila
 {
@@ -33,6 +35,7 @@ namespace sakila
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<sakilaContext>(options => options.UseSqlServer(Configuration.GetConnectionString("sakila")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
